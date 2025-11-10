aeca7f7fd77ef4b916fe9c5f16af5629f7a7428d
RufusFranky <151731451+RufusFranky@users.noreply.github.com>
2025-11-10 22:20:38 +0530

    file change

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const { Pool } = pg;
 
-//Use only the connection string in production
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // required for Neon DB
+  ssl: { rejectUnauthorized: false }, /
 });
 
 export default pool;
